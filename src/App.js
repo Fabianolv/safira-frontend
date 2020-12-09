45c3c53224beeaaf0216b7b2960b9e4dfdbb6794
Fabiano Luiz Vieira da Silva <CIANDT\fabianolv@lnb021895bhz.ciandt.global>
2020-12-09 16:01:36 -0300

    [Implementaçao Backend] Buscando dados do endpoint GET '/customers' utilizando axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
+
+
 import './App.css';
+import './Sidebar.css';
+import './global.css';
+import './Main.css';
+
+import { useEffect, useState } from 'react';
+import api from './config/api';
 
 function App() {
+  const [customers, setCustumers] = useState([]);
+
+  useEffect(() => {
+    async function loadCustumers() { 
+        const customers = await api.get('/customer');
+
+        setCustumers(customers.data);
+    }
+
+    loadCustumers();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <form>
+            <div className="input-block">
+              <label>
+                First Name: 
+              </label>
+              <input type="text" />
+            </div>
+            <div className="input-block">
+              <label>
+                Last Name:
+              </label>
+              <input type="text" />
+            </div>
+            <div className="input-block">
+              <label>
+                Birthday: 
+              </label>
+              <input type="date" />
+            </div>
+            <button type="submit">Cadastrar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          {customers.map(customer => (
+            <li key={customer.id} className="customer-item">
+              <header>
+                <strong>{`${customer.firstName} ${customer.lastName}`}</strong>
+                <span><strong>Birthday: </strong>{customer.birthday}</span>
+              </header>
+              <div className="account-info">
+                <strong>Created At: </strong>
+                <span>{customer.createdAt}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
