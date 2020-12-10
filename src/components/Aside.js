d3b38b8354a30480af63237cf925540ac126320e
Fabiano Luiz Vieira da Silva <CIANDT\fabianolv@lnb021895bhz.ciandt.global>
2020-12-10 14:11:22 -0300

    [Aplicação] adicionando função POST no formulario utilizando axios

---
+++ src/components/Aside.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import api from '../config/api';
+import Input from './Input';
+
+function Aside() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  console.log(birthday);
+
+  function onSubmit() {
+    api.post("/customer", {
+      firstName: firstName,
+      lastName: lastName,
+      birthday: birthday.split('-').reverse().join('/')
+    })
+    .then((respose) => console.log(respose))
+    .catch(e => console.error(e));
+  }
+
+  return (
+    <aside>
+      <form onSubmit={onSubmit}>
+        <Input 
+          label="First Name" 
+          type="text" 
+          value={firstName} 
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input 
+          label="Last Name" 
+          type="text" 
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <Input 
+          label="Birthday" 
+          type="date" 
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </aside>
+  )
+}
+
+export default Aside;
